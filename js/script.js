752df5e29e3746bfee84cfd614d8afd0ef597994
Jordan Brauner <jordan.brauner@gmail.com>
2015-10-26 15:04:08 -0400

    Recreated wireframe

--- js/script.js
+++ js/script.js
@@ -1,22 +1,89 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  var chances;
-  // var matched = [];
-
-  // created tiles
-  var createTiles = function () {
+  // create tile divs
+  var createTileDivs = function() {
     for (var i = 0; i < 16; i ++) {
-      $('#tile-wrapper').append("<div class='tile' id=\""+i+"tile\"></div>");
+      $('#tile-wrapper').append("<div class='tile'></div>");
+      // $('#tile-wrapper').append("<div class='tile' id=\""+i+"tile\"></div>");
     }
     $('.tile').append("<p id='tileBack'>C</p>")
   }
-  //
-  // $('.tile').on('click', function () {
-  //   $(this).
-  // });
+  createTileDivs();
 
-  createTiles();
-
-
-
+  // On click, reveal tile
 });
+
+var guesses = 4;
+$('#guesses').append(guesses);
+
+var deck = [
+    "red",
+    "red",
+    "blue",
+    "blue",
+    "yellow",
+    "yellow",
+    "green",
+    "green",
+    "aqua",
+    "aqua",
+    "slategray",
+    "slategray",
+    "lemonchiffon",
+    "lemonchiffon",
+    "rebeccapurple",
+    "rebeccapurple"
+];
+
+var usedTiles = [];
+
+
+// for (var i = 0; usedTiles.length < 17; i++) {
+//   usedTiles.push(deck[i]);
+//   // $("\"#"+i+"tile\"").css("background-color", deck[i]);
+// }
+
+// $('#tile-yellow').on('click', function () {
+//   $(this).attr('background', 'yellow');
+// });
+
+/************************
+OLD CODE
+************************/
+
+// var storeColor = function(color) {
+//   $('#2tile').css("background-color", color);
+// }
+
+
+// random: function () {
+//     var random = Math.random();
+//     for (var i = 0; i < 16; i++) {
+//       var ranNum = 0;
+//       var rng = function () {
+//         if (random < 0.12) {
+//             var ranNum = this.deck[0];
+//         }
+//         else if (random < 0.24) {
+//             var ranNum = this.deck[1];
+//         }
+//         else if (random < 0.36) {
+//             var ranNum = this.deck[2];
+//         }
+//         else if (random < 0.48) {
+//             var ranNum = this.deck[3];
+//         }
+//         else if (random < 0.60) {
+//             var ranNum = this.deck[4];
+//         }
+//         else if (random < 0.72) {
+//             var ranNum = this.deck[5];
+//         }
+//         else if (random < 0.84) {
+//             var ranNum = this.deck[6];
+//         }
+//         else {
+//             var ranNum = this.deck[7];
+//         }
+//       }
+//     }
