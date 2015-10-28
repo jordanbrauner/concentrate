5107bbc72ab26599476800c36216df037e2a70b5
Jordan Brauner <jordan.brauner@gmail.com>
2015-10-28 11:56:03 -0400

    Updated with code from Master branch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
+  // Start splash screen
+  // $('#board').hide();
+  $('#small-logo-1').hide();
+  $('#small-logo-2').hide();
+  $('.game-title').text("READY?");
+
   // declare variables
+  var countdownNum = 3;
   var clicks = 0;
   var matched = 0;
   var match1;
@@ -10,9 +17,78 @@ $(document).ready(function() {
   var matchElementColor;
   var wrong;
   var lives = 4;
-  var sound = new Audio();
-  sound.src = "../images/sfx.mp3";
+  // var dingSFX = $('#dingSound')[0];
+  // var matchSFX = $('#matchSound')[0];
   $('#lives').text(lives);
+
+  // Sound effects
+  // $(".hide").mouseenter(function() {
+  //   dingSFX.play();
+  // });
+
+  // Chooses a random quote
+  var randomQuote = function () {
+    var random = Math.random();
+    if (random < 0.20) {
+      $('.quote').text("Democracy is a beautiful thing, except for that part about letting just any old yokel vote.\"");
+    }
+    else if (random < 0.40) {
+      $('.quote').text("If trees could scream, would we be so cavalier about cutting them down? We might, if they screamed all the time, for no good reason.\"");
+    }
+    else if (random < 0.60) {
+      $('.quote').text("When you go in for a job interview, I think a good thing to ask is if they ever press charges\"");
+    }
+    else if (random < 0.80) {
+      $('.quote').text("If you ever accidentally drop your keys into a river of molten lava, let 'em go, because man, they're gone.\"");
+    }
+    else {
+      $('.quote').text("Children need encouragement. If a kid gets an answer right, tell him it was a lucky guess. That way he develops a good, lucky feeling.\"");
+    }
+  }
+  randomQuote();
+
+  // Begin game when user clicks on splash-image
+  $('#splash-image').on('click', function() {
+    $('#splash-image').hide();
+    $('#small-logo-1').show();
+    $('#small-logo-2').show();
+    setInterval(countdown, 1000);
+  });
+
+  // Resets game when clicking on game-title
+  $('.start').on('click', function() {
+    countdownNum = 0;
+    lives = 4;
+    $('#lives').text(lives);
+    setInterval(countdown, 1000);
+  });
+
+  // Countdown until show board and startgame are called
+  var countdown = function() {
+    if (countdownNum > 0) {
+      $('.game-title').text(countdownNum);
+      countdownNum -= 1;
+    }
+    else if (countdownNum === 0) {
+      begin();
+      countdownNum -=1;
+    }
+  }
+
+  // reveals board then starts game
+  // BUG: countdown continues to loop as clearInterval is not working
+  var begin = function() {
+    if (countdownNum === 0) {
+      clearInterval(countdown);
+      $('.game-title').text("CONCENTRATE!");
+      showBoard();
+      setTimeout(startGame, 1000);
+      // console.log('running countdown else');
+    }
+    else {
+      countdown();
+    }
+  }
 
   // shows all the colors briefly at the start of the game
   var showBoard = function () {
@@ -21,35 +97,13 @@ $(document).ready(function() {
     $('.hide').toggleClass('hide');
   }
 
-  // runs when an incorrect match is made
-  var wrongMatch = function () {
-    $(matchElement1).toggleClass('hide');
-    $(matchElement2).toggleClass('hide');
-    lives -= 1;
-    $('#lives').text(lives);
-    clicks = 0;
-    console.log(lives);
-    $('.game-title').text('PICK ANOTHER!');
-    if (lives === 0) {
-      $('.game-title').text('TRY AGAIN?');
-      $('.game-title').toggleClass('start');
-    }
-  }
-
-  // runs when a correct match is made
-  var correctMatch = function () {
-    $('.game-title').text('PICK ANOTHER!');
-  }
-
   // starts the game
   var startGame = function () {
+    clearInterval(countdown);
     $('.game-title').text('TAKE YOUR PICK!');
     $('#tile-wrapper div').toggleClass('hide')
     $('.hide').on('click', function() {
-    if (matched === 8) {
-      console.log('You Win');
-    }
-    else if ((lives !== 0) && (matched !== 8)) {
+    if ((lives !== 0) && (matched !== 8)) {
       if (clicks === 0) {
         $(this).toggleClass('hide');
         matchElement1 = $(this);
@@ -67,6 +121,7 @@ $(document).ready(function() {
           matchElementColor = matchElement2.css('background-color');
           $("#match-icon-"+matched).css('background-color', matchElementColor);
           setTimeout(correctMatch, 1000);
+          console.log(matched);
         }
         else {
           $('.game-title').text('NOPE!');
@@ -76,12 +131,40 @@ $(document).ready(function() {
     }
   });
   }
+
+  // runs when a correct match is made
+  var correctMatch = function () {
+    if (matched < 8) {
+      $('.game-title').text('PICK ANOTHER!');
+    }
+    else {
+      $('.game-title').text('CONGRATULATIONS!');
+    }
+  }
+
+  // runs when an incorrect match is made
+  var wrongMatch = function () {
+    $(matchElement1).toggleClass('hide');
+    $(matchElement2).toggleClass('hide');
+    lives -= 1;
+    $('#lives').text(lives);
+    clicks = 0;
+    console.log(lives);
+    $('.game-title').text('PICK ANOTHER!');
+    if (lives === 0) {
+      $('.game-title').text('TRY AGAIN?');
+      $('.game-title').toggleClass('start');
+    }
+  }
+
   //shows the colors in the tiles then hides them
-  $('.start').on('click', function() {
-    // $('#start-screen').hide();
-    showBoard();
-    setTimeout(startGame, 4000);
-  });
+  // $('.start').on('click', function() {
+  //   $('#splash-image').hide();
+  //   $('#small-logo-1').show();
+  //   $('#small-logo-2').show();
+  //   showBoard();
+  //   setTimeout(startGame, 1000);
+  // });
 });
 
 
